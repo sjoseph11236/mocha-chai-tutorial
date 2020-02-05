af841bc1f958ebc0f31e6ad4ab2c6fb00c41af60
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-04 20:37:15 -0500

    seprate describe blocks for each function

--- test/index_test.js
+++ test/index_test.js
@@ -2,26 +2,34 @@
 const assert = require('chai').assert;
 const { sayHello, addNumbers } = require('../index');
 
+const sayHelloResult = sayHello();
+const addNumbersResult = addNumbers(5,5);
+
 describe('index', function() {
-  it('sayHello should return hello', function() {
-    let result = sayHello();
-    assert.equal(result , 'hello');
+
+  describe('sayHelo()', function() {
+    it('sayHello should return hello', function() {
+      // let result = sayHello();
+      assert.equal(sayHelloResult , 'hello');
+    });
+  
+    it('sayHello should return type string', function() {
+      // let result = sayHello();
+      assert.typeOf(sayHelloResult, 'string');
+    });
   });
 
-  it('sayHello should return type string', function() {
-    let result = sayHello();
-    assert.typeOf(result, 'string');
-  });
-
-  it('addNumbers should be above 5', function() {
-    let result = addNumbers(5, 5);
-    assert.isAbove(result, 5);
-  })
-
-
-  it('andNumbers should return type number', function() {
-    let result = addNumbers(5, 5);
-    assert.typeOf(result, 'number');
+  describe('addNumbers()', function() {
+    it('addNumbers should be above 5', function() {
+      // let result = addNumbers(5, 5);
+      assert.isAbove(addNumbersResult, 5);
+    })
+  
+  
+    it('andNumbers should return type number', function() {
+      // let result = addNumbers(5, 5);addNumbersResult
+      assert.typeOf(addNumbersResult, 'number');
+    });
   });
 });
 
